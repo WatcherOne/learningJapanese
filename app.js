cd1d907aa65e9464eaf7610c4d617f7e1b79ea23
zhubo <286154864@qq.com>
2023-08-16 16:50:17 +0800

    add some files and test

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-// app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+    onLaunch () {
+        console.log(333)
+        wx.login({
+          success: res => {
+            const { code } = res
+            console.log(1, code)
+            const url = `https://api.weixin.qq.com/sns/jscode2session?appid=wx16d26cbba526afec&secret=4492fb9cf413d8d66bd894e0e2cfdac4&js_code=${code}&grant_type=authorization_code`
+            wx.request({
+              url,
+              header: {
+                'content-type': 'application/json'
+              },
+              success: res => {
+                const openid = res.data.openid
+                console.log(openid)
+              }
+            })
+          }
+        })
+        wx.authorize({
+            scope: 'scope.userInfo',
+            success: () => {
+                wx.getUserProfile({
+                  desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+                  success: (res) => {
+                      console.log(res.userInfo)
+                      this.setData({
+                          userInfo: res.userInfo,
+                          hasUserInfo: true
+                      })
+                  }
+                })
+            }
+        })
+    },
+    globalData: {
+        userInfo: null
+    }
 })
